ce01e61ecf12131b1209bb48b160069303fa6b08
Alexander Kraev <aleksandr.kraev@lenta.com>
2024-06-07 00:54:30 +0300

    no message

--- iOS example/iOS example/SBPay.swift
+++ iOS example/iOS example/SBPay.swift
@@ -8,14 +8,15 @@ final class SBPay {
                     onBankChoose: @escaping (String) -> (),
                     onClose: @escaping () -> ()) {
         
-        let vc = UIHostingController(rootView: BanksView() {
-            onBankChoose($0)
-            viewController.presentedViewController?.dismiss(animated: true)
-        } onCloseTap: {
-            onClose()
-            viewController.presentedViewController?.dismiss(animated: true)
-        })
-        vc.modalPresentationStyle = presentationStyle
-        viewController.present(vc, animated: true)
-    }
+      registerFonts()
+      let vc = UIHostingController(rootView: BanksView() {
+          onBankChoose($0)
+          viewController.presentedViewController?.dismiss(animated: true)
+      } onCloseTap: {
+          onClose()
+          viewController.presentedViewController?.dismiss(animated: true)
+      })
+      vc.modalPresentationStyle = presentationStyle
+      viewController.present(vc, animated: true)
+  }
 }
